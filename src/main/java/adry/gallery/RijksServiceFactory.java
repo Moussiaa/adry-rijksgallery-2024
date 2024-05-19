633a7b3c65ec35e3144e168ef6f516bc7fce12c8
Moussiaa <moussiaadry@gmail.com>
2024-05-19 15:20:27 -0400

    checkstyles

--- src/main/java/adry/gallery/RijksServiceFactory.java
+++ src/main/java/adry/gallery/RijksServiceFactory.java
@@ -3,7 +3,9 @@ package adry.gallery;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RijksServiceFactory {
+
     // configure Retrofit for the website
     public RijksService getService() {
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://www.rijksmuseum.nl/en")
